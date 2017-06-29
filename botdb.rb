2a34a501a8d099ac297d51f2857385fa34a153d9
Steve Netting <steve@netting.org.uk>
2017-06-29 15:32:39 +0000

    commented botdb

--- botdb.rb
+++ botdb.rb
@@ -1,5 +1,4 @@
 
-
 class DB
 
   def initialize(db)
@@ -47,7 +46,7 @@ class DB
     #p "downcased query: #{@stermstring}"
     #load "bot_blockwords.rb"
     #@blockwords = ["tcmbot:", " a ", " an ", " and ", " are ", " as ", " at ", " be ", " by ", " for ", " from ", " has ", " he ", " in ", " is ", " it ", " of ", " on ", " that ", " the ", " to ", " was ", " were ", " will ", " with ", " my "]
-    @blockwords = ["tcmbot:", "a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","cannot","can't","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","her","here","here's","hers","herself","he's","him","himself","his","how","how's","i","i'd","if","i'll","i'm","in","into","is","isn't","it","its","it's","itself","i've","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","were","we're","weren't","we've","what","what's","when","when's","where","where's","which","while","who","whom","who's","why","why's","with","won't","would","wouldn't","you","you'd","you'll","your","you're","yours","yourself","yourselves","you've"]
+    @blockwords = ["a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","cannot","can't","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","her","here","here's","hers","herself","he's","him","himself","his","how","how's","i","i'd","if","i'll","i'm","in","into","is","isn't","it","its","it's","itself","i've","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","were","we're","weren't","we've","what","what's","when","when's","where","where's","which","while","who","whom","who's","why","why's","with","won't","would","wouldn't","you","you'd","you'll","your","you're","yours","yourself","yourselves","you've"]
     #@blockwords.each { |word| @stermstring.gsub!(word," ") }
     @blockwords.each { |word| @stermstring.gsub!( /\b#{Regexp.escape(word)}s?\b/, " " ) }
     @sterms = @stermstring.split(" ")
@@ -76,6 +75,7 @@ class DB
     end
 
     # Match highscore to index and report value
+    # If we have > 1 results matching highscore, choose at random
     #p "Searching for highscore #{highscore}..."
     p "High score: #{@highscore}"
     @rpointer = 0
